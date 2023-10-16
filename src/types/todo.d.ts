b032a921e3d84b2f3b5d4849d0aea8ca8ee54859
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-10-16 23:59:32 +0900

    add sidenav

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -2,8 +2,18 @@ export type Todo = {
   id: number
   text: string
   completed: boolean
+  labels: Label[]
 }
 
 export type NewTodoPayload = {
   text: string
+}
+
+export type Label = {
+  id: number
+  name: string
+}
+
+export type NewLabelPayload = {
+  name: string
 }
